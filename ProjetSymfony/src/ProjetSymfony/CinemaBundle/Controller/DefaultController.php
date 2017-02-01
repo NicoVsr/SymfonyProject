927102134ec986db1092160da27e2a639b384480
Unknown <nicolas.vess@gmail.com>
2017-02-01 11:40:01 +0100

    Ajout BDD + Entity

--- ProjetSymfony/src/ProjetSymfony/CinemaBundle/Controller/DefaultController.php
+++ ProjetSymfony/src/ProjetSymfony/CinemaBundle/Controller/DefaultController.php
@@ -1,5 +1,5 @@
 <?php
-
+// src/ProjetSymfony/CinemaBundle/Controller/DefaultController.php
 namespace ProjetSymfony\CinemaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,5 +13,22 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('ProjetSymfonyCinemaBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/films")
+     */
+    public function listAction()
+    {
+        $films = $this->getDoctrine()->getRepository('ProjetSymfonyCinemaBundle:Film')->findAll();
+        return $this->render('ProjetSymfonyCinemaBundle:Film:list.html.twig');
+    }
+
+    /**
+     * @Route("/film/{id}", requirements={"id": "\d+"})
+     */
+    public function showAction($id)
+    {
+        return $this->render('ProjetSymfonyCinemaBundle:Film:show.html.twig');
     }
 }
