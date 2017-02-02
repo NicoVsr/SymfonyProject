05581fe5a996c1bf249cea3893a901dc14cc4ec1
Unknown <nicolas.vess@gmail.com>
2017-02-02 15:49:05 +0100

    Mise en place du HTML/CSS

--- ProjetSymfony/src/ProjetSymfony/CinemaBundle/Controller/DefaultController.php
+++ ProjetSymfony/src/ProjetSymfony/CinemaBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/",name="page_accueil")
      */
     public function indexAction()
     {
@@ -16,19 +16,29 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/films")
+     * @Route("/films", name="page_films")
      */
     public function listAction()
     {
         $films = $this->getDoctrine()->getRepository('ProjetSymfonyCinemaBundle:Film')->findAll();
-        return $this->render('ProjetSymfonyCinemaBundle:Film:list.html.twig');
+        $titre_de_la_page = 'Films de la bibliothèques';
+
+        return $this->render(
+            'ProjetSymfonyCinemaBundle:Film:list.html.twig',
+            ['films' => $films,'titre' => $titre_de_la_page]
+        );
     }
 
     /**
-     * @Route("/film/{id}", requirements={"id": "\d+"})
+     * @Route("/film/{id}", requirements={"id": "\d+"}, name="page_film")
      */
     public function showAction($id)
     {
-        return $this->render('ProjetSymfonyCinemaBundle:Film:show.html.twig');
+        $film = $this->getDoctrine()->getRepository('ProjetSymfonyCinemaBundle:Film')->find($id);
+
+        return $this->render(
+            'ProjetSymfonyCinemaBundle:Film:show.html.twig',
+            ['film' => $film]
+        );
     }
 }
